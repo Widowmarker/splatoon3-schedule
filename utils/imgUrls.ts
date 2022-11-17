bb14c0038990f39346152aa50eaa0c50fb5f3eda
DJ <704472894@qq.com>
2022-11-17 22:50:26 +0800

    fix:使用压缩后图片

--- utils/imgUrls.ts
+++ utils/imgUrls.ts
@@ -1,19 +1,38 @@
+// const mapUrl = {
+// 	'Q29vcFN0YWdlLTI=': 'https://cdnjson.com/images/2022/11/15/SockeyeStation.png', // 新卷堡
+// 	'Q29vcFN0YWdlLTc=': 'https://cdnjson.com/images/2022/11/15/GoneFissionHydroplant.png', // 发电站
+// 	'Q29vcFN0YWdlLTE=': 'https://cdnjson.com/images/2022/11/15/SpawningGrounds.png', // 大坝
+// 	'VnNTdGFnZS0x': 'https://cdnjson.com/images/2022/11/15/ScorchGorge.png', // 温泉花大峡谷
+// 	'VnNTdGFnZS0xMA==': 'https://cdnjson.com/images/2022/11/15/HammerheadBridge.png', // 真鲭跨海大桥
+// 	'VnNTdGFnZS0xMQ==': 'https://cdnjson.com/images/2022/11/15/MuseumdAlfonsino.png', // 金眼鲷美术馆
+// 	'VnNTdGFnZS0xMg==': 'https://cdnjson.com/images/2022/11/15/Mahi-MahiResort.png', // 鬼头刀SPA度假区
+// 	'VnNTdGFnZS0xMw==': 'https://cdnjson.com/images/2022/11/15/InkblotArtAcademy.png', // 海女美术大学
+// 	'VnNTdGFnZS0xNA==': 'https://cdnjson.com/images/2022/11/15/SturgeonShipyard.png', // 鲟鱼造船厂
+// 	'VnNTdGFnZS0xNQ==': 'https://cdnjson.com/images/2022/11/15/MakoMart.png', // 座头购物中心
+// 	'VnNTdGFnZS0xNg==': 'https://cdnjson.com/images/2022/11/15/WahooWorld.png', // 醋饭海洋世界
+// 	'VnNTdGFnZS0y': 'https://cdnjson.com/images/2022/11/15/EeltailAlley.png', // 鳗鲶区
+// 	'VnNTdGFnZS0z': 'https://cdnjson.com/images/2022/11/15/HagglefishMarket.png', // 烟管鱼市场
+// 	'VnNTdGFnZS00': 'https://cdnjson.com/images/2022/11/15/UndertowSpillway.png', // 竹蛏疏洪道
+// 	'VnNTdGFnZS02': 'https://cdnjson.com/images/2022/11/15/MincemeatMetalworks.png', // 鱼肉碎金属
+// }
+
+// 50%jpg
 const mapUrl = {
-	'Q29vcFN0YWdlLTI=': 'https://cdnjson.com/images/2022/11/15/SockeyeStation.png', // 新卷堡
-	'Q29vcFN0YWdlLTc=': 'https://cdnjson.com/images/2022/11/15/GoneFissionHydroplant.png', // 发电站
-	'Q29vcFN0YWdlLTE=': 'https://cdnjson.com/images/2022/11/15/SpawningGrounds.png', // 大坝
-	'VnNTdGFnZS0x': 'https://cdnjson.com/images/2022/11/15/ScorchGorge.png', // 温泉花大峡谷
-	'VnNTdGFnZS0xMA==': 'https://cdnjson.com/images/2022/11/15/HammerheadBridge.png', // 真鲭跨海大桥
-	'VnNTdGFnZS0xMQ==': 'https://cdnjson.com/images/2022/11/15/MuseumdAlfonsino.png', // 金眼鲷美术馆
-	'VnNTdGFnZS0xMg==': 'https://cdnjson.com/images/2022/11/15/Mahi-MahiResort.png', // 鬼头刀SPA度假区
-	'VnNTdGFnZS0xMw==': 'https://cdnjson.com/images/2022/11/15/InkblotArtAcademy.png', // 海女美术大学
-	'VnNTdGFnZS0xNA==': 'https://cdnjson.com/images/2022/11/15/SturgeonShipyard.png', // 鲟鱼造船厂
-	'VnNTdGFnZS0xNQ==': 'https://cdnjson.com/images/2022/11/15/MakoMart.png', // 座头购物中心
-	'VnNTdGFnZS0xNg==': 'https://cdnjson.com/images/2022/11/15/WahooWorld.png', // 醋饭海洋世界
-	'VnNTdGFnZS0y': 'https://cdnjson.com/images/2022/11/15/EeltailAlley.png', // 鳗鲶区
-	'VnNTdGFnZS0z': 'https://cdnjson.com/images/2022/11/15/HagglefishMarket.png', // 烟管鱼市场
-	'VnNTdGFnZS00': 'https://cdnjson.com/images/2022/11/15/UndertowSpillway.png', // 竹蛏疏洪道
-	'VnNTdGFnZS02': 'https://cdnjson.com/images/2022/11/15/MincemeatMetalworks.png', // 鱼肉碎金属
+	'Q29vcFN0YWdlLTI=': 'https://cdnjson.com/images/2022/11/17/SockeyeStation6fa86f86e6cad0aa.jpg', // 新卷堡
+	'Q29vcFN0YWdlLTc=': 'https://cdnjson.com/images/2022/11/17/GoneFissionHydroplant28c157dbfd57cacb.jpg', // 发电站
+	'Q29vcFN0YWdlLTE=': 'https://cdnjson.com/images/2022/11/17/SpawningGrounds25d51dc50a608e1a.jpg', // 大坝
+	'VnNTdGFnZS0x': 'https://cdnjson.com/images/2022/11/17/ScorchGorgeacc7229fea6e7000.jpg', // 温泉花大峡谷
+	'VnNTdGFnZS0xMA==': 'https://cdnjson.com/images/2022/11/17/HammerheadBridgefedeb010e6345b0e.jpg', // 真鲭跨海大桥
+	'VnNTdGFnZS0xMQ==': 'https://cdnjson.com/images/2022/11/17/MuseumdAlfonsino4f8e0855a0c7a770.jpg', // 金眼鲷美术馆
+	'VnNTdGFnZS0xMg==': 'https://cdnjson.com/images/2022/11/17/Mahi-MahiResort2c271ef02c8a77ed.jpg', // 鬼头刀SPA度假区
+	'VnNTdGFnZS0xMw==': 'https://cdnjson.com/images/2022/11/17/InkblotArtAcademyd31d58ac12860218.jpg', // 海女美术大学
+	'VnNTdGFnZS0xNA==': 'https://cdnjson.com/images/2022/11/17/SturgeonShipyard07ebe85f8a2a84af.jpg', // 鲟鱼造船厂
+	'VnNTdGFnZS0xNQ==': 'https://cdnjson.com/images/2022/11/17/MakoMart59717dc8e1e5fa06.jpg', // 座头购物中心
+	'VnNTdGFnZS0xNg==': 'https://cdnjson.com/images/2022/11/17/WahooWorlddc0091ba3e07f7cd.jpg', // 醋饭海洋世界
+	'VnNTdGFnZS0y': 'https://cdnjson.com/images/2022/11/17/EeltailAlley3375a3c9d53bf1a4.jpg', // 鳗鲶区
+	'VnNTdGFnZS0z': 'https://cdnjson.com/images/2022/11/17/HagglefishMarkete891a89346f1beb3.jpg', // 烟管鱼市场
+	'VnNTdGFnZS00': 'https://cdnjson.com/images/2022/11/17/UndertowSpillwayb91bb543e25a77f8.jpg', // 竹蛏疏洪道
+	'VnNTdGFnZS02': 'https://cdnjson.com/images/2022/11/17/MincemeatMetalworks9d81568b57dfeefe.jpg', // 鱼肉碎金属
 }
 
 export { mapUrl }
